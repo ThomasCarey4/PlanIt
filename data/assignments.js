de77d2fd8d127de70302e64632606d96daaa972e
planit <planit@tanjeffreyz.github.io>
2023-11-20 10:40:07 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -77,6 +77,13 @@ assignments = {
       "submitted": true
     },
     {
+      "title": "week 08 submission - table.html",
+      "course": "202324_39156_COMP1012",
+      "dueDate": "2023-12-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/550772",
+      "submitted": false
+    },
+    {
       "title": "week 03 submission - gradescope_test.py",
       "course": "202324_39156_COMP1012",
       "dueDate": "2023-10-22T23:59:00",
