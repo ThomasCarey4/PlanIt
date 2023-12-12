b40116d6419308e80f0eb25498714576b134157f
planit <planit@tanjeffreyz.github.io>
2023-12-12 23:37:51 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -50,7 +50,7 @@ assignments = {
       "title": "Part 2 - Task 3 - Outputting Sorted Data",
       "course": "202324_32439_COMP1711",
       "dueDate": "2023-12-13T14:00:00",
-      "link": "https://www.gradescope.com/courses/658287/assignments/3706750/submissions/219779440",
+      "link": "https://www.gradescope.com/courses/658287/assignments/3706750/submissions/219787819",
       "submitted": true
     }
   ],
