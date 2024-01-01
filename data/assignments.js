b459078b7d11e589e2a759827e02c62b2961496b
planit <planit@tanjeffreyz.github.io>
2024-01-01 01:45:17 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -3,14 +3,14 @@ assignments = {
     {
       "title": "Coursework 1",
       "course": "202324_32435_COMP1211",
-      "dueDate": "2023-11-16T14:00:00",
+      "dueDate": "2024-11-16T14:00:00",
       "link": "https://www.gradescope.com/courses/633850/assignments/3410906/submissions/209861362",
       "submitted": true
     },
     {
       "title": "Coursework 2",
       "course": "202324_32435_COMP1211",
-      "dueDate": "2023-12-14T14:00:00",
+      "dueDate": "2024-12-14T14:00:00",
       "link": "https://www.gradescope.com/courses/633850/assignments/3430347/submissions/218350948",
       "submitted": true
     }
@@ -19,14 +19,14 @@ assignments = {
     {
       "title": "Coursework 1",
       "course": "202324_32437_COMP1421",
-      "dueDate": "2023-11-02T10:00:00",
+      "dueDate": "2024-11-02T10:00:00",
       "link": "https://www.gradescope.com/courses/640079",
       "submitted": false
     },
     {
       "title": "Coursework 2",
       "course": "202324_32437_COMP1421",
-      "dueDate": "2023-12-07T10:00:00",
+      "dueDate": "2024-12-07T10:00:00",
       "link": "https://www.gradescope.com/courses/640079",
       "submitted": false
     }
@@ -35,21 +35,21 @@ assignments = {
     {
       "title": "COMP1711 - Part 1 Task 1",
       "course": "202324_32439_COMP1711",
-      "dueDate": "2023-11-03T14:05:00",
+      "dueDate": "2024-11-03T14:05:00",
       "link": "https://www.gradescope.com/courses/658287/assignments/3583378/submissions/205999941",
       "submitted": true
     },
     {
       "title": "Part 2 - Task 2 - Analysing Our Data",
       "course": "202324_32439_COMP1711",
-      "dueDate": "2023-12-13T14:00:00",
+      "dueDate": "2024-12-13T14:00:00",
       "link": "https://www.gradescope.com/courses/658287/assignments/3706746/submissions/219641085",
       "submitted": true
     },
     {
       "title": "Part 2 - Task 3 - Outputting Sorted Data",
       "course": "202324_32439_COMP1711",
-      "dueDate": "2023-12-13T14:00:00",
+      "dueDate": "2024-12-13T14:00:00",
       "link": "https://www.gradescope.com/courses/658287/assignments/3706750/submissions/219820555",
       "submitted": true
     }
@@ -58,91 +58,91 @@ assignments = {
     {
       "title": "week 05 submission - unimodule.py",
       "course": "202324_39156_COMP1012",
-      "dueDate": "2023-11-12T23:59:00",
+      "dueDate": "2024-11-12T23:59:00",
       "link": "https://www.gradescope.com/courses/550772/assignments/2943641/submissions/205688325",
       "submitted": true
     },
     {
       "title": "week 04 submission[optional] - function_exercise_q3.py",
       "course": "202324_39156_COMP1012",
-      "dueDate": "2023-10-29T23:59:00",
+      "dueDate": "2024-10-29T23:59:00",
       "link": "https://www.gradescope.com/courses/550772",
       "submitted": false
     },
     {
       "title": "week 04 submission - function_exercise_q4.py",
       "course": "202324_39156_COMP1012",
-      "dueDate": "2023-10-29T23:59:00",
+      "dueDate": "2024-10-29T23:59:00",
       "link": "https://www.gradescope.com/courses/550772/assignments/2943644/submissions/205673853",
       "submitted": true
     },
     {
       "title": "week 06  submission - summary.py",
       "course": "202324_39156_COMP1012",
-      "dueDate": "2023-11-19T23:59:00",
+      "dueDate": "2024-11-19T23:59:00",
       "link": "https://www.gradescope.com/courses/550772/assignments/2943662/submissions/212485789",
       "submitted": true
     },
     {
       "title": "week 06 submission[optional] - caesar.py",
       "course": "202324_39156_COMP1012",
-      "dueDate": "2023-11-19T23:59:00",
+      "dueDate": "2024-11-19T23:59:00",
       "link": "https://www.gradescope.com/courses/550772",
       "submitted": false
     },
     {
       "title": "week 07 submission - ducky.py",
       "course": "202324_39156_COMP1012",
-      "dueDate": "2023-12-03T23:59:00",
+      "dueDate": "2024-12-03T23:59:00",
       "link": "https://www.gradescope.com/courses/550772/assignments/2943671/submissions/212491128",
       "submitted": true
     },
     {
       "title": "week 08 submission - table.html",
       "course": "202324_39156_COMP1012",
-      "dueDate": "2023-12-03T23:59:00",
+      "dueDate": "2024-12-03T23:59:00",
       "link": "https://www.gradescope.com/courses/550772/assignments/2949565/submissions/215461876",
       "submitted": true
     },
     {
       "title": "week 0910 submission[optional] - calculate_grade.html",
       "course": "202324_39156_COMP1012",
-      "dueDate": "2023-12-17T23:59:00",
+      "dueDate": "2024-12-17T23:59:00",
       "link": "https://www.gradescope.com/courses/550772",
       "submitted": false
     },
     {
       "title": "week 0910 submission - bill_sharing.html.html",
       "course": "202324_39156_COMP1012",
-      "dueDate": "2023-12-17T23:59:00",
+      "dueDate": "2024-12-17T23:59:00",
       "link": "https://www.gradescope.com/courses/550772",
       "submitted": false
     },
     {
       "title": "week 03 submission - gradescope_test.py",
       "course": "202324_39156_COMP1012",
-      "dueDate": "2023-10-22T23:59:00",
+      "dueDate": "2024-10-22T23:59:00",
       "link": "https://www.gradescope.com/courses/550772/assignments/2951009/submissions/205244324",
       "submitted": true
     },
     {
       "title": "Coursework 1 submission",
       "course": "202324_39156_COMP1012",
-      "dueDate": "2023-11-02T10:00:00",
+      "dueDate": "2024-11-02T10:00:00",
       "link": "https://www.gradescope.com/courses/550772/assignments/3030283/submissions/207988458",
       "submitted": true
     },
     {
       "title": "Coursework 2 submission",
       "course": "202324_39156_COMP1012",
-      "dueDate": "2023-12-14T10:00:00",
+      "dueDate": "2024-12-14T10:00:00",
       "link": "https://www.gradescope.com/courses/550772/assignments/3030294/submissions/219819063",
       "submitted": true
     },
     {
       "title": "perfecting_my_cwk1_code",
       "course": "202324_39156_COMP1012",
-      "dueDate": "2023-12-15T23:59:00",
+      "dueDate": "2024-12-15T23:59:00",
       "link": "https://www.gradescope.com/courses/550772",
       "submitted": false
     }
