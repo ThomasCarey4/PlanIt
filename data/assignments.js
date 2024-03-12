af002be1b212c3cac9f4efd99b959306ddc1234d
planit <planit@tanjeffreyz.github.io>
2024-03-12 16:39:09 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -20,8 +20,8 @@ assignments = {
       "title": "Coursework 1 Parts (d-h)",
       "course": "202324_32436_COMP1212",
       "dueDate": "2024-03-14T10:00:00",
-      "link": "https://www.gradescope.com/courses/735969/assignments/4095142/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/735969/assignments/4095142/submissions/238482212",
+      "submitted": true
     }
   ],
   "202324_32438_COMP1511": [
