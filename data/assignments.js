6a763493f352bc46d0b62bc206e4f03ec7c835c3
planit <planit@tanjeffreyz.github.io>
2024-06-29 15:37:46 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,1 +1,95 @@
-assignments = {};
+assignments = {
+  "202324_32434_COMP1121": [
+    {
+      "title": "Coursework1",
+      "course": "202324_32434_COMP1121",
+      "dueDate": "2024-03-14T10:00:00",
+      "link": "https://www.gradescope.com/courses/712711/assignments/4072378/submissions/238809945",
+      "submitted": true
+    }
+  ],
+  "202324_32436_COMP1212": [
+    {
+      "title": "Coursework 1 Parts a-c",
+      "course": "202324_32436_COMP1212",
+      "dueDate": "2024-03-14T10:00:00",
+      "link": "https://www.gradescope.com/courses/735969/assignments/4095081/submissions/238776530",
+      "submitted": true
+    },
+    {
+      "title": "Coursework 1 Parts (d-h)",
+      "course": "202324_32436_COMP1212",
+      "dueDate": "2024-03-14T10:00:00",
+      "link": "https://www.gradescope.com/courses/735969/assignments/4095142/submissions/238482212",
+      "submitted": true
+    },
+    {
+      "title": "Coursework II: Assembly Language Programming 23/24",
+      "course": "202324_32436_COMP1212",
+      "dueDate": "2024-05-09T10:00:00",
+      "link": "https://www.gradescope.com/courses/735969/assignments/4243061/submissions/250626560",
+      "submitted": true
+    }
+  ],
+  "202324_32438_COMP1511": [
+    {
+      "title": "Coursework 1",
+      "course": "202324_32438_COMP1511",
+      "dueDate": "2024-02-23T17:00:00",
+      "link": "https://www.gradescope.com/courses/680728/assignments/3785467/submissions/233201005",
+      "submitted": true
+    },
+    {
+      "title": "Coursework 2",
+      "course": "202324_32438_COMP1511",
+      "dueDate": "2024-03-08T17:00:00",
+      "link": "https://www.gradescope.com/courses/680728/assignments/3785488/submissions/236695282",
+      "submitted": true
+    },
+    {
+      "title": "Coursework 3",
+      "course": "202324_32438_COMP1511",
+      "dueDate": "2024-03-22T17:00:00",
+      "link": "https://www.gradescope.com/courses/680728/assignments/3785506/submissions/240170131",
+      "submitted": true
+    },
+    {
+      "title": "Coursework 4",
+      "course": "202324_32438_COMP1511",
+      "dueDate": "2024-05-03T17:01:00",
+      "link": "https://www.gradescope.com/courses/680728/assignments/3785509/submissions/251115775",
+      "submitted": true
+    }
+  ],
+  "202324_32441_COMP1911": [],
+  "202324_32442_COMP1921": [
+    {
+      "title": "Assignment 1 - Test Script and Code",
+      "course": "202324_32442_COMP1921",
+      "dueDate": "2024-02-29T16:00:00",
+      "link": "https://www.gradescope.com/courses/718272/assignments/4107453/submissions/235293355",
+      "submitted": true
+    },
+    {
+      "title": "Assignment 2 - Maze Game",
+      "course": "202324_32442_COMP1921",
+      "dueDate": "2024-05-02T16:00:00",
+      "link": "https://www.gradescope.com/courses/718272/assignments/4230930/submissions/251413132",
+      "submitted": true
+    },
+    {
+      "title": "Assignment 2 - Extension - Mazegen",
+      "course": "202324_32442_COMP1921",
+      "dueDate": "2024-05-02T16:00:00",
+      "link": "https://www.gradescope.com/courses/718272/assignments/4230935/submissions/251530188",
+      "submitted": true
+    },
+    {
+      "title": "RESIT STUDENTS ONLY - Assignment 2 Maze game",
+      "course": "202324_32442_COMP1921",
+      "dueDate": "2024-05-02T16:00:00",
+      "link": "https://www.gradescope.com/courses/718272",
+      "submitted": false
+    }
+  ]
+};
