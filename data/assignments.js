d180da77d465a5e9ea8005afba7e869a031db8b1
planit <planit@tanjeffreyz.github.io>
2024-09-30 07:38:27 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,53 @@
 assignments = {
-  "202425_32879_COMP2421": []
+  "202425_32879_COMP2421": [
+    {
+      "title": "Portfolio section 1",
+      "course": "202425_32879_COMP2421",
+      "dueDate": "2024-10-15T14:00:00",
+      "link": "https://www.gradescope.com/courses/873347/assignments/4998996/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Portfolio section 2",
+      "course": "202425_32879_COMP2421",
+      "dueDate": "2024-10-22T14:00:00",
+      "link": "https://www.gradescope.com/courses/873347/assignments/5002375/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Portfolio section 3",
+      "course": "202425_32879_COMP2421",
+      "dueDate": "2024-11-05T14:00:00",
+      "link": "https://www.gradescope.com/courses/873347/assignments/5002417/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Portfolio section 4",
+      "course": "202425_32879_COMP2421",
+      "dueDate": "2024-11-12T14:00:00",
+      "link": "https://www.gradescope.com/courses/873347/assignments/5002443/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Portfolio section 5",
+      "course": "202425_32879_COMP2421",
+      "dueDate": "2024-11-19T14:00:00",
+      "link": "https://www.gradescope.com/courses/873347/assignments/5002463/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Portfolio section 6",
+      "course": "202425_32879_COMP2421",
+      "dueDate": "2024-11-26T14:00:00",
+      "link": "https://www.gradescope.com/courses/873347/assignments/5002485/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Portfolio section 7",
+      "course": "202425_32879_COMP2421",
+      "dueDate": "2024-12-03T14:00:00",
+      "link": "https://www.gradescope.com/courses/873347/assignments/5002489/submissions/new",
+      "submitted": false
+    }
+  ]
 };
