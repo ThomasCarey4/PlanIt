cca6eb5454c5b724b76d219150b362c93ea352fe
planit <planit@tanjeffreyz.github.io>
2024-10-18 09:40:13 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -20,8 +20,8 @@ assignments = {
       "title": "Portfolio section 2",
       "course": "202425_32879_COMP2421",
       "dueDate": "2024-10-22T14:00:00",
-      "link": "https://www.gradescope.com/courses/873347/assignments/5002375/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/873347/assignments/5002375/submissions/279302023",
+      "submitted": true
     },
     {
       "title": "Portfolio section 3",
