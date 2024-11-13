d64a4b9405f35f0a2e600ce73ce375b61001348b
planit <planit@tanjeffreyz.github.io>
2024-11-13 10:42:42 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -119,6 +119,13 @@ assignments = {
       "submitted": false
     },
     {
+      "title": "Preparation for Tutorial 6",
+      "course": "202425_32881_COMP2711",
+      "dueDate": "2024-11-19T11:00:00",
+      "link": "https://www.gradescope.com/courses/876566/assignments/5082754/submissions/new",
+      "submitted": false
+    },
+    {
       "title": "Coursework 1",
       "course": "202425_32881_COMP2711",
       "dueDate": "2024-11-06T14:00:00",
