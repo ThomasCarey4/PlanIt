9e78f4694cd609c9823cdf0ce4c4bb0cfba96a8b
planit <planit@tanjeffreyz.github.io>
2025-01-22 02:02:10 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,4 +1,12 @@
 assignments = {
-  "202425_32875_COMP2221": [],
+  "202425_32875_COMP2221": [
+    {
+      "title": "Worksheet 1 Question 1",
+      "course": "202425_32875_COMP2221",
+      "dueDate": "2025-09-08T17:00:00",
+      "link": "https://www.gradescope.com/courses/961993",
+      "submitted": false
+    }
+  ],
   "202425_35699_COMP2912": []
 };
