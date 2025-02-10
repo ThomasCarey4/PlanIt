e548d12030004d90c4cb557fec8afbe5e502b2ee
planit <planit@tanjeffreyz.github.io>
2025-02-10 09:39:55 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -15,7 +15,15 @@ assignments = {
       "submitted": false
     }
   ],
-  "202425_32880_COMP2611": [],
+  "202425_32880_COMP2611": [
+    {
+      "title": "Search,  Adv Games and KRR questions for exam revision",
+      "course": "202425_32880_COMP2611",
+      "dueDate": "2025-12-31T09:00:00",
+      "link": "https://www.gradescope.com/courses/975231/assignments/5759465/submissions/new",
+      "submitted": false
+    }
+  ],
   "202425_32882_COMP2721": [],
   "202425_35699_COMP2912": []
 };
